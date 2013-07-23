9830788c1a74722b8e6f860fc1903eee7a266a2a
EpiphanyMachine <EpiphanyMachine@gmail.com>
2013-07-23 12:32:18 -0700

    added jQuery and d3

--- public/lib/bootstrap.js
+++ public/lib/bootstrap.js
@@ -1,5 +1,5 @@
 /**
-* Bootstrap.js by @fat & @mdo
+* Bootstrap.js by @fat & @mdo v2.3.2
 * plugins: bootstrap-transition.js, bootstrap-modal.js, bootstrap-dropdown.js, bootstrap-scrollspy.js, bootstrap-tab.js, bootstrap-tooltip.js, bootstrap-popover.js, bootstrap-affix.js, bootstrap-alert.js, bootstrap-button.js, bootstrap-collapse.js, bootstrap-carousel.js, bootstrap-typeahead.js
 * Copyright 2012 Twitter, Inc.
 * http://www.apache.org/licenses/LICENSE-2.0.txt
